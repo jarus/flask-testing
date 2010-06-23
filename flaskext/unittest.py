93c6365ce23debbd31f7e361146cd172163393ee
Dan Jacob <danjac354@gmail.com>
2010-06-23 08:38:56 +0100

    first commit

---
+++ flaskext/unittest.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+    flaskext.unittest
+    ~~~~~~~~~~~~~~~~~
+
+    Description of the module goes here...
+
+    :copyright: (c) 2010 by Dan Jacob.
+    :license: BSD, see LICENSE for more details.
+"""
+
+import unittest
+
+class TestCase(unittest.TestCase):
+
+    def create_app(self):
+        """
+        Create your Flask app here, with any
+        configuration you need
+        """
+        raise NotImplementedError
+
+    def __call__(self, result=None):
+       """
+       Does the required setup, doing it here
+       means you don't have to call super.setUp
+       in subclasses.
+       """
+       self._pre_setup()
+       super(TestCase, self).__call__(result)
+       self._post_tearDown()
+
+    def _pre_setup(self):
+       self.app = self.create_app()
+       self.client = self.app.test_client()
+       
+       # now you can use flask thread locals
+
+       self._ctx = self.app.test_request_context()
+       self._ctx.push()
+
+    def _post_tearDown(self):
+       self._ctx.pop()
+
+    def assertRedirects(self, response, location):
+        assert response.status_code in (301, 302)
+        assert response.location == "http://localhost" + location
+
+    def assertStatus(self, response, status_code):
+        self.assertEqual(response.status_code, status_code)
+
+    def assert200(self, response):
+        self.assertStatus(response, 200)
+
+    def assert404(self, response):
+        self.assertStatus(response, 404)
---
+++ setup.py
@@ -0,0 +1,45 @@
+"""
+flask-unittest
+--------------
+
+Description goes here...
+
+Links
+`````
+
+* `documentation <http://packages.python.org/flask-unittest>`_
+* `development version
+  <http://bitbucket.org/USERNAME/REPOSITORY/get/tip.gz#egg=flask-unittest-dev`_
+
+
+"""
+from setuptools import setup
+
+
+setup(
+    name='flask-unittest',
+    version='0.1',
+    url='<enter URL here>',
+    license='BSD',
+    author='Dan Jacob',
+    author_email='your-email-here@example.com',
+    description='<enter short description here>',
+    long_description=__doc__,
+    packages=['flaskext'],
+    namespace_packages=['flaskext'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
