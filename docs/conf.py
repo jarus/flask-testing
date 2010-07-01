ea3e3ea1c2abb001c43ab612b0b1e3242dc8f1ea
Dan Jacob <danjac354@gmail.com>
2010-07-01 21:46:48 +0100

    doc fixes

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'flask-unittest'
+project = u'Flask-Testing'
 copyright = u'2010, Dan Jacob'
 
 # The version info for the project you're documenting, acts as replacement for
