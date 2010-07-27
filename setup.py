055336a78a21f8c4d7d00a7faa91ce0f32479dee
Dan Jacob <danjac354@gmail.com>
2010-07-27 09:06:14 +0100

    0.2.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Testing',
-    version='0.2.1',
+    version='0.2.2',
     url='http://bitbucket.org/danjac/flask-testing',
     license='BSD',
     author='Dan Jacob',
