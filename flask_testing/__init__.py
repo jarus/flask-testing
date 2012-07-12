6f02542e5f8ff3a8eaca58a8a4e92d55b638a286
Christoph Heer <Christoph.Heer@gmail.com>
2012-07-12 10:43:37 +0200

    Add module.__doc__

--- flask_testing/__init__.py
+++ flask_testing/__init__.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+    flask_testing
+    ~~~~~~~~~~~~~
+
+    Flask unittest integration.
+
+    :copyright: (c) 2010 by Dan Jacob.
+    :license: BSD, see LICENSE for more details.
+"""
+
 from __future__ import absolute_import
 
 from .utils import TestCase
